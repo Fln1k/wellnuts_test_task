3066d9e4c19c76a738643c4d9fe781341c801798
Sergei <43087751+Fln1k@users.noreply.github.com>
2019-12-09 20:45:33 +0300

    formatted version of code added

--- matching_brackets.exs
+++ matching_brackets.exs
@@ -9,26 +9,26 @@ defmodule BracketsBalance do
   def add_to_stack(_, []), do: :error
 
   def add_to_stack(stack, [current_symbol | rest_string]) do
-        if Map.has_key?(@brackets, current_symbol) do
-          add_to_stack([current_symbol] ++ stack, rest_string)
+    if Map.has_key?(@brackets, current_symbol) do
+      add_to_stack([current_symbol] ++ stack, rest_string)
+    else
+      if current_symbol == @brackets[Enum.at(stack, 0)] do add_to_stack(tl(stack), rest_string)
+      else
+        if Map.values(@brackets) |> Enum.member?(current_symbol) do
+            :error
         else
-            if current_symbol == @brackets[Enum.at(stack, 0)] do add_to_stack(tl(stack), rest_string)
-            else
-                if Map.values(@brackets) |> Enum.member?(current_symbol) do
-                    :error
-                else
-                    add_to_stack(stack, rest_string)
-                end           
-            end
-        end 	
+            add_to_stack(stack, rest_string)
+        end           
+      end
+    end   
   end
 
   def check(text) do
     IO.write(text <> " is_balanced? ")
     with :ok <- add_to_stack([], String.codepoints(text)) do
-        IO.puts true
+      IO.puts true
     else
-        :error -> IO.puts false     
+      :error -> IO.puts false     
     end
   end
 end
